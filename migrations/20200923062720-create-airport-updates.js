2a889dd3175dae1f0301a2e3e428a8184c4db085
ja <you@example.com>
2020-09-23 09:23:37 +0200

    trigger done

---
+++ migrations/20200923062720-create-airport-updates.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('AirportUpdates', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      oldAirport: {
+        type: Sequelize.STRING
+      },
+      newAirport: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('AirportUpdates');
+  }
+};
---
+++ models/airportupdates.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class AirportUpdates extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  AirportUpdates.init({
+    country: DataTypes.STRING,
+    oldAirport: DataTypes.STRING,
+    newAirport: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'AirportUpdates',
+  });
+  return AirportUpdates;
+};
